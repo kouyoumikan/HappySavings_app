63d45dc457518042090ab058265963efddec1dcc
kouyoumikan <kouyoumikan136@gmail.com>
2020-10-08 16:01:27 +0900

    下部ナビゲーションバーのスタイル設定を追加

--- happy_savings/lib/screens/create/create.dart
+++ happy_savings/lib/screens/create/create.dart
@@ -19,43 +19,54 @@ class _CreateState extends State<Create> {
       body: Center(
         child: Text('Next'),
       ),
-      // 下部ナビゲーションバーでスクロール
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // アイコンの色をハイライト表示
-        items: [
-          // BottomNavigationBarItemに表示するリスト
-          BottomNavigationBarItem(
-            icon: Icon(Icons.create),
-            title: Text("入力"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            title: Text("カレンダー"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text("設定"),
-          ),
-        ],
-        // ナビゲーションバーのアイコンをタップしたときの処理
-        onTap: (int index) {
-          setState(
-            () {
-              // _navIndex = index;
-              // _label = _titles[index];
+      // 下部ナビゲーションバーのスタイル指定
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+            // 下部ナビゲーションバーの背景色
+            canvasColor: Colors.grey[100],
+            // 下部ナビゲーションバーのアイコンの色設定
+            primaryColor: Colors.blue, // アイコンの色をハイライト表示
+            textTheme: Theme.of(context)
+                .textTheme
+                // 未選択のアイコンの色
+                .copyWith(caption: new TextStyle(color: Colors.grey))),
+        // 下部ナビゲーションバーでスクロール
+        child: new BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: 1, // 現在選択しているアイコンの色をハイライト表示
+            items: [
+              // BottomNavigationBarItemに表示するリスト
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("ホーム"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.create),
+                title: Text("入力"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                title: Text("カレンダー"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text("設定"),
+              ),
+            ],
+            onTap: (int index) {
               print(index); // デバッグ用に出力（タップされたボタンによって数値がかわる）
               if (index == 0) {
                 // First Icon が押されたときは前の画面に戻る
                 // Navigator.of(context).pop();
-                // Navigator.of(context).pushNamed("/create");
+                Navigator.of(context).pushNamed("/");
               } else if (index == 1) {
-                Navigator.of(context).pushNamed("/calendar");
+                Navigator.of(context).pushNamed("/create");
               } else if (index == 2) {
+                Navigator.of(context).pushNamed("/calendar");
+              } else if (index == 3) {
                 Navigator.of(context).pushNamed("/setting");
               }
-            },
-          );
-        },
+            }),
       ),
     );
   }
